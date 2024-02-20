22ae71b942992cd935d73b8a14271a2ea2248392
MagicTrashPanada <cmc5039@gmail.com>
2024-02-20 16:24:47 -0500

    Initial Commit

---
+++ Panda Pal.py
@@ -0,0 +1,31 @@
+# import discord and settings
+from settings import *
+import discord
+from discord import app_commands
+# declare bot intents
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+# define client and tree for slash commands
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+# first command
+@tree.command(
+    name="ping",
+    description="My first application Command! It checks latency!",
+    guild=discord.Object(id=836717870905163806)
+)
+async def ping(interaction):
+    await interaction.user.send(f'Pong! {round(client.latency * 1000)} ms.')
+    await interaction.response.send_message('Sent to your dms!')
+
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=836717870905163806))
+    print(f'We have logged in as {client.user}')
+
+client.run(discordToken)
